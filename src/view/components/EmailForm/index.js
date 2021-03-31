795a5caa7150851b0c26441641519ff33f66280f
suyeon <tndus7644@gmail.com>
2021-03-31 21:45:58 +0900

    upload components

--- src/view/components/EmailForm/index.js
+++ src/view/components/EmailForm/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import {Back} from "../../../svg";
 import {ContentContainer, InputStyle, PageContainer} from "../../../styled/Common";
--- src/view/pages/Upload/index.js
+++ src/view/pages/Upload/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {ContentContainer, PageContainer} from "../../../styled/Common";
+import {ContentContainer, InputStyle, PageContainer} from "../../../styled/Common";
 import {navigate} from "../../../lib/History";
 import {Back} from "../../../svg";
 import {GlobalButton} from "../../../styled/Button.Styled";
@@ -19,9 +19,38 @@ const Upload = () => {
                 </Header>
             </ContentContainer>
             <Photo><h4>사진을 등록하세요.</h4></Photo>
-            <ContentContainer>
-
-            </ContentContainer>
+            <UploadContent>
+                <Name>
+                    <label>
+                        <h1>제품명</h1>
+                        <InputStyle
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="제품명 입력"/>
+                    </label>
+                </Name>
+                <Price>
+                    <label>
+                        <h1>가격</h1>
+                        <InputStyle
+                            type="text"
+                            id="price"
+                            name="price"
+                            placeholder="가격 입력"/>
+                    </label>
+                </Price>
+                <Detail>
+                    <label>
+                        <h1>상세 설명</h1>
+                        <InputStyle
+                            rows={"10"}
+                            cols={"50"}
+                            name="detail"
+                            placeholder="상세 설명 입력"/>
+                    </label>
+                </Detail>
+            </UploadContent>
         </Container>
     )
 }
@@ -58,9 +87,41 @@ const Photo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  h4{
+
+  h4 {
     color: ${props => props.theme.GrayScale06};
     font-size: ${props => props.theme.H4};;
+  }
+`;
+
+const UploadContent = styled(ContentContainer)`
+  margin-top: 20px;
+
+  h1 {
+    font-size: ${props => props.theme.H1};
+    font-weight: 600;
+    margin-bottom: 10px;
+  }
+
+  div {
+    margin-bottom: 24px;
+  }
+`;
+
+const Name = styled.div`
+
+`;
+
+const Price = styled.div`
+
+`;
+
+const Detail = styled.div`
+  height: 100%;
+
+  input {
+    width: 100%;
+    height: 256px;
   }
 `;
 
